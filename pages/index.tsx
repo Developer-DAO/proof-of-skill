8ef40abbd78fb7ef8c3f7ea002911878bade03dc
Greg Syme <gjsyme@gmail.com>
2021-10-18 09:41:46 -0500

    Make a basic Ceramic interaction (#10)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from "next"
+import dynamic from 'next/dynamic';
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { useTranslation } from "react-i18next"
+import { Box, Text } from '@chakra-ui/react';
+
+const CeramicComponent = dynamic (
+  () => import('../components/CeramicComponent'),
+  {ssr: false}
+);
 
 const Home: NextPage = () => {
-  const { t } = useTranslation()
-  return <div>{t("title")}</div>
+  const { t } = useTranslation();
+  return (
+    <Box>
+      <Text>{t("title")}</Text>
+      <CeramicComponent/>  
+    </Box>
+  )
 }
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
